0214b6242ea1b832114593bb7e47c809d9e89e88
shanifcm <mohammedshanifcm@gmail.com>
2026-02-01 11:47:03 +0530

    levelsadded

---
+++ Assets/shanif/fallingObstacles.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FallingObstacle : MonoBehaviour
+{
+    public int damage = 1;
+
+    private Rigidbody2D rb;
+    private Vector3 startPos;
+    private bool hasFallen = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        startPos = transform.position;
+        rb.gravityScale = 0;
+    }
+
+    public void Fall()
+    {
+        if (hasFallen) return;
+
+        hasFallen = true;
+        rb.gravityScale = 2; // start falling
+    }
+
+  
+}
+
