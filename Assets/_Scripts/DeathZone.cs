a53a2bc17514033446bf4088bcdca9914a47c023
shanifcm <mohammedshanifcm@gmail.com>
2026-02-01 10:31:27 +0530

    enemy
    respawns when enemy hits

---
+++ Assets/_Scripts/DeathZone.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathZone : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            string currentScene = SceneManager.GetActiveScene().name;
+            FindObjectOfType<SceneFader>().FadeToScene(currentScene);
+        }
+    }
+}
